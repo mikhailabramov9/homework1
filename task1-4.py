f27807d1db3c7b36a59526f76f3e17f7283298b7
mikhailabramov9 <mikhailabramov1983@gmail.com>
2024-11-02 17:35:32 +0200

    Add files via upload

---
+++ task1-4.py
@@ -0,0 +1,12 @@
+"""1st program"""
+print((9**0.5)*5)
+"2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+"3rd program"
+print(2*2 +2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+"4th program"
+name = "123.456"
+print(name[4]) # я бы так сделал, если тока не требуется выводить первое число после зпт любого введеного числа
+print(int(float(name)*10) % 10)
